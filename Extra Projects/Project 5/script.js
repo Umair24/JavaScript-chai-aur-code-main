afb00215ea9fb6f93ed5e217c240fc90107e9501
Umair Ansari <ansariumair2402@gmail.com>
2024-02-04 14:52:29 +0530

    project 5

---
+++ Extra Projects/Project 5/script.js
@@ -0,0 +1,40 @@
+let arr = [{
+    dp: "https://images.unsplash.com/photo-1706564621641-883b71497cdb?q=80&w=1903&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://plus.unsplash.com/premium_photo-1675490807666-001dfb161a83?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+{
+    dp: "https://images.unsplash.com/photo-1706621927383-c212d9477535?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://images.unsplash.com/photo-1706034021225-c72d1311697e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+{
+    dp: "https://images.unsplash.com/photo-1682685797229-b2930538da47?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://plus.unsplash.com/premium_photo-1703806517546-581595edd4e4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+{
+    dp: "https://plus.unsplash.com/premium_photo-1706564641805-835e081e227f?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://images.unsplash.com/photo-1706549668584-8edd79c8f7ae?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+{
+    dp: "https://images.unsplash.com/photo-1706548195448-a6d8969cae6a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://images.unsplash.com/photo-1706016868884-4d0e348a1647?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+]
+
+let storiyan = document.querySelector("#storiyan")
+let clutter = ""
+arr.forEach(function(elem, idx){
+    clutter += `<div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+</div>`
+})
+
+storiyan.innerHTML = clutter
+
+
+storiyan.addEventListener("click", function(dets){
+    document.querySelector("#full-screen").style.display = "block"
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(function(){
+    document.querySelector("#full-screen").style.display = "none"
+    },3000)
+})
